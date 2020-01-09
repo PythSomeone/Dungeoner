ec71a52fc988d574400a371e4f412994c79b2e08
Mateusz <mateuszjacenty1@gmail.com>
2020-01-09 18:32:19 +0100

    Changes in hero object

--- Characters/Hero.java
+++ Characters/Hero.java
@@ -10,41 +10,36 @@ public class Hero extends Character {
 		super(name, health, mana, strenght, agility, inteligence, luck, toughness);	
 	}
 	
+	private String title;
 	private int requiredExp;
-	private int currentExp;
+	private int currentExp = 0;
+	private int statAmount = 0;
+	private int baseHealthIncrease = 5;
+	private int baseManaIncrease = 5;
+	private String[] availableAttributes = {"strenght","agility","inteligence","luck","toughness"};
 	
-	
-	public void levelUp(int healthIncrease, int manaIncrease, int statAmount)
+	public void levelUp()
 	{
-		level++;
-		this.maxHealth += healthIncrease;
-		this.maxMana += manaIncrease;
+		this.maxHealth += level * baseHealthIncrease;
+		this.maxMana += level * baseManaIncrease;
 		this.currentHealth = this.maxHealth;
 		this.currentMana = this.maxMana;
+		this.statAmount += 5;
+		level++;
 		
 		Log.info("You've leveled up!  (+" 
-		+ healthIncrease + " health | +" 
-		+ manaIncrease + " mana)");
-		
-		// !!!HERE NEEDS TO BE CHOOSING STATISTICS TO ADD!!!
+		+ level * baseHealthIncrease + " health | +" 
+		+ level * baseManaIncrease + " mana)");		
+	}
+	
+	public void distributePoints() {
 		Scanner scan = new Scanner(System.in);
-		String[] availableAttributes = {"strenght","agility","inteligence","luck","toughness"};
 		
-		for(int i = 0; i < statAmount; i++) {
-			Log.info("Choose attribute ( Strenght, Agility, Inteligence, Luck, Toughness ) :");
-			
+		for(int i = 0; i < this.statAmount; i++) {
+			Log.info("Choose attribute ( " + printAvailableAttributes() + ") :");
 			
 			String choice = scan.nextLine();
 			
-			/*
-			for(;;) {
-				choice = scan.nextLine();
-				for(String x : availableAttributes) {
-					if(choice.equalsIgnoreCase(x))
-						break;
-				}
-			}
-			*/
 			if(choice.equalsIgnoreCase("strenght")) this.strenght++;
 			else if(choice.equalsIgnoreCase("agility")) this.agility++;
 			else if(choice.equalsIgnoreCase("inteligence")) this.inteligence++;
@@ -54,10 +49,18 @@ public class Hero extends Character {
 				Log.info("Skill point have not been distributed. Try again!");
 				i--;
 			}
-
+			Log.info("You've added 1 point to " + choice.toUpperCase());
 		}
-		
+		Log.info("");
 	}
 	
+	public String printAvailableAttributes() {
+		String sentence = "";
+		for(String attribute : availableAttributes) {
+			sentence += "["+attribute.toUpperCase()+"] ";
+
+		}
+		return sentence;	
+	}
 
 }
--- Main.java
+++ Main.java
@@ -9,15 +9,29 @@ public class Main
 {
 public static void main(String[] args) 
 {
+	// INTRODUCTION //
+	
+	// INTRODUCTION //
+	
+	
 	// GAME SETUP //
 	Scanner skan1=new Scanner(System.in);
-	Log.info("Whats ur name?");	
+	Log.info("Whats Your name?");	
 	String name = skan1.nextLine();
-	Log.info("Hello "+ name);
 	
 	Hero hero = new Hero(name,50,50,5,5,5,5,5);
+	Log.info("Hello "+ name + "!");
+	hero.distributePoints();
 	// GAME SETUP //
 	
 	
+	// START GAME //
+	hero.levelUp(4, 4, 1);
+	hero.levelUp(4, 4, 1);
+	hero.distributePoints();
+	// START GAME //
+	
+	//FIGHTSTATES//
+	// 1 - 
 }
 }
