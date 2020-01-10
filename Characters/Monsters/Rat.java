fe34ac82c46af3adc5fb46892d71513dab65a84c
Someone <Someone@DESKTOP-KNSBOLB.(none)>
2020-01-10 18:25:01 +0100

    New monsters added

---
+++ Characters/Monsters/Rat.java
@@ -0,0 +1,11 @@
+package Characters.Monsters;
+
+import Characters.Monster;
+
+public class Rat extends Monster {
+
+	public Rat(String name, int health, int mana, int strenght, int agility, int inteligence, int luck, int toughness, int level) {
+		super(name, health, mana, strenght, agility, inteligence, luck, toughness, level);	
+	}
+	
+}
