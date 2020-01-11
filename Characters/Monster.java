97ff8c2dfc016591029a19aa81e4e45e71ca4e5e
Someone <Someone@DESKTOP-KNSBOLB.(none)>
2020-01-12 00:12:33 +0100

    Random ability added

--- Characters/Monster.java
+++ Characters/Monster.java
@@ -1,6 +1,7 @@
 package Characters;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 import CombatSystem.Ability;
@@ -8,8 +9,7 @@ import GameEngine.Log;
 
 public class Monster extends Character {
 	
-	public Monster(String name, int health, int mana, int strenght, int agility, int inteligence, int luck,
-			int toughness) {
+	public Monster(String name, int health, int mana, int strenght, int agility, int inteligence, int luck,int toughness,int level) {
 		super(name, health, mana, strenght, agility, inteligence, luck, toughness);
 	}
 
@@ -22,6 +22,10 @@ public class Monster extends Character {
 		abilities.add(new Ability(name,multiplier,manaUsage));
 	}
 	
-	
+	public Ability randomAbility() {
+		Collections.shuffle(abilities);
+		Ability abilty=abilities.get(0);
+		return abilty;
+	}
 
 }
