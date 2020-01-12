e6276b37ed3a39adea70a8a2d3333a19d5bf153c
Someone <Someone@DESKTOP-KNSBOLB.(none)>
2020-01-12 14:05:32 +0100

    New abilities

--- Characters/Monsters/Ghul.java
+++ Characters/Monsters/Ghul.java
@@ -1,11 +1,16 @@
 package Characters.Monsters;
 
 import Characters.Monster;
+import CombatSystem.Ability;
 
 public class Ghul extends Monster {
 
 	public Ghul(String name, int health, int mana, int strenght, int agility, int inteligence, int luck, int toughness, int level) {
-		super(name, health, mana, strenght, agility, inteligence, luck, toughness, level);	
+		super(name, health, mana, strenght, agility, inteligence, luck, toughness, level);
+		abilities.add(new Ability("Slice", 1, 5));
+		abilities.add(new Ability("Slash", 2, 50));
+		abilities.add(new Ability("Hack", 3, 250));
 	}
 	
+
 }
