f7d8a394b4353cd2cebe568ed83ab1b50b33334d
Mateusz <mateuszjacenty1@gmail.com>
2020-01-14 10:40:04 +0100

    No comment

--- CombatSystem/Combat.java
+++ CombatSystem/Combat.java
@@ -1,7 +1,6 @@
 package CombatSystem;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import Characters.Character;
 import Characters.Hero;
@@ -13,14 +12,14 @@ import GameEngine.Log;
 
 public class Combat {
 	
-	public Hero player;
-	public Monster enemy;
-	public Character winner;
+	private Hero player;
+	private Monster enemy;
+	private Character winner;
 
 	public Combat(Hero player, Monster enemy) {
 		
-		this.player=player;
-		this.enemy=enemy;
+		this.player = player;
+		this.enemy = enemy;
 		
 		Log.info("Your current oponnent : ");
 		enemy.printCharacter();
@@ -52,8 +51,6 @@ public class Combat {
 	public void playerAttack(Hero player,Monster enemy){
 		ArrayList<Ability> abilities = player.getAbilities();
 		
-		
-		Scanner scan = new Scanner(System.in);
 		String option;
 		boolean success = false;
 		boolean actionDone = false;
@@ -62,7 +59,7 @@ public class Combat {
 		while (!success || !actionDone) {
 			Log.info("Choose ability id to attack an enemy ");
 			player.printAbilityList();
-			option = scan.nextLine();
+			option = Log.scanString();
 			try {
 				opt = Integer.parseInt(option);
 				if(opt <= abilities.size()+1 && opt > 0) success = true;
@@ -79,21 +76,21 @@ public class Combat {
 						int damage = 0;
 						if(player.hasEnoughMana(ability)) {
 							if(player instanceof Assassin) {
-								damage = ability.getMultiplier()*(player.getAgility()+player.getWeapon().getStats())-enemy.getToughness();
+								damage = ability.getMultiplier() * (player.getAgility() + player.getWeapon().getStats()) - enemy.getToughness();
 								Log.info(player.getName() + " use " + ability.getName());
 								enemy.dealDamage(damage);
 								player.burnMana(ability.getManaUsage());
 								actionDone = true;
 							}
 							else if(player instanceof Warrior) {
-								damage = ability.getMultiplier()*(player.getStrenght()+player.getWeapon().getStats())-enemy.getToughness();
+								damage = ability.getMultiplier() * (player.getStrenght() + player.getWeapon().getStats()) - enemy.getToughness();
 								Log.info(player.getName() + " use " + ability.getName());
 								enemy.dealDamage(damage);
 								player.burnMana(ability.getManaUsage());
 								actionDone = true;
 							}
 							else if(player instanceof Mage) {
-								damage = ability.getMultiplier()*(player.getInteligence()+player.getWeapon().getStats())-enemy.getToughness();
+								damage = ability.getMultiplier() * (player.getInteligence() + player.getWeapon().getStats()) - enemy.getToughness();
 								Log.info(player.getName() + " use " + ability.getName());
 								enemy.dealDamage(damage);
 								player.burnMana(ability.getManaUsage());
@@ -107,18 +104,19 @@ public class Combat {
 	
 	public void enemyAttack(Hero player,Monster enemy){
 		Ability randomAbility = enemy.getRandomAbility();
-		int damage=(randomAbility.getMultiplier()*enemy.getStrenght())-player.getArmor().getStats();
+		int damage = (randomAbility.getMultiplier() * enemy.getStrenght()) - player.getArmor().getStats();
 		Log.info(enemy.getName() + " use " + randomAbility.getName());
 		player.dealDamage(damage);
 	}
 	
 	public void getWinner() {
-		Log.info(winner.getName()+" has won");
+		Log.info(winner.getName() + " has won");
 		Log.pressAnyKeyToContinue();
 	}
 	
 	public void setWinner(Character winner) {
 		this.winner=winner;
 	}
+
 	
 }
